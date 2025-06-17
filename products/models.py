a39c0677cc0b05d75ed0ccf1ec0eca8702f34621
TylerRTDev <TylerRTDev@outlook.com>
2025-06-17 22:48:57 +0100

    Update: Add function to define objects as strings in Admin panel. Stops generic model object nameing convention

--- products/models.py
+++ products/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 class Genre(models.Model):
     name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
 
 class MediaType(models.Model):
     name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
