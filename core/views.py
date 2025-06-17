ffaf18f780ce062ddb61eb09b48f23c72ccbe035
TylerRTDev <TylerRTDev@outlook.com>
2025-06-17 22:24:48 +0100

    Update: Add genre and media type grid to homepage with category tiles

--- core/views.py
+++ core/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from products.models import Genre, MediaType, Product
 
 def home(request):
-    return render(request, 'core/home.html')
+    featured = Product.objects.filter(is_featured=True)[:4]
+    genres = Genre.objects.all()
+    media_types = MediaType.objects.all()
+    return render(request, 'core/home.html', {
+        'featured': featured,
+        'genres': genres,
+        'media_types': media_types,
+    })
