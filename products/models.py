1d8483c930c520ca67a17037218494929b04bc89
TylerRTDev <TylerRTDev@outlook.com>
2025-06-17 16:16:17 +0100

    Update: Products app model updated. New classes for Genre, Media Type, Product and Product Image

--- products/models.py
+++ products/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+class MediaType(models.Model):
+    name = models.CharField(max_length=100)
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)
+    media_type = models.ForeignKey(MediaType, on_delete=models.SET_NULL, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    is_featured = models.BooleanField(default=False)
+    is_limited_edition = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='products/')
+    is_main = models.BooleanField(default=False)
