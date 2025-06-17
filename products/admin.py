d6604dd5043bf7142b7153b8903eb1886b1ddcfb
TylerRTDev <TylerRTDev@outlook.com>
2025-06-17 17:09:15 +0100

    Update: Product model has been registered

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Genre, MediaType, Product, ProductImage
 
-# Register your models here.
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'genre', 'media_type', 'price', 'stock', 'is_featured', 'is_limited_edition', 'created_at')
+    list_filter = ('genre', 'media_type', 'is_featured', 'is_limited_edition')
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductImageInline]
+
+admin.site.register(Genre)
+admin.site.register(MediaType)
