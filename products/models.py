7c1c94a1fe439a4c64aa0676d4866c8eb83523ad
TylerRTDev <TylerRTDev@outlook.com>
2025-06-19 15:18:17 +0100

    Test: Update Product model to add category for more specific filtering.

--- products/models.py
+++ products/models.py
@@ -12,9 +12,22 @@ class MediaType(models.Model):
     def __str__(self):
         return self.name
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            from django.utils.text import slugify
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 class Product(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     description = models.TextField()
     genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)
     media_type = models.ForeignKey(MediaType, on_delete=models.SET_NULL, null=True)
@@ -23,6 +36,7 @@ class Product(models.Model):
     is_featured = models.BooleanField(default=False)
     is_limited_edition = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
