c9dc91d4c760d3b5cd36804e39f266fabe9019fc
TylerRTDev <TylerRTDev@outlook.com>
2025-06-20 13:25:29 +0100

    Update: Add cart quantity update functionality using session and POST form

--- cart/urls.py
+++ cart/urls.py
@@ -6,4 +6,5 @@ app_name = 'cart'
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('remove/<str:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('update/', views.update_cart_quantity, name='update_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.views.decorators.http import require_POST
 
 def view_cart(request):
     cart = request.session.get('cart', {})
@@ -31,4 +32,22 @@ def remove_from_cart(request, item_id):
         del cart[item_id]
         request.session['cart'] = cart
         request.session.modified = True
+    return redirect('cart:view_cart')
+
+@require_POST
+def update_cart_quantity(request):
+    item_id = request.POST.get('item_id')
+    quantity = int(request.POST.get('quantity', 1))
+
+    cart = request.session.get('cart', {})
+
+    if item_id in cart:
+        if quantity > 0:
+            cart[item_id]['quantity'] = quantity
+        else:
+            del cart[item_id]
+
+        request.session['cart'] = cart
+        request.session.modified = True
+
     return redirect('cart:view_cart')
