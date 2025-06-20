f7a5bc9d94598d41fbf191e45fafa535b5be7038
TylerRTDev <TylerRTDev@outlook.com>
2025-06-20 04:53:51 +0100

    Update: Add responsive navigation bar with links to home and shop pages, add category to admin filter

--- products/admin.py
+++ products/admin.py
@@ -7,8 +7,8 @@ class ProductImageInline(admin.TabularInline):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'genre', 'media_type', 'price', 'stock', 'is_featured', 'is_limited_edition', 'created_at')
-    list_filter = ('genre', 'media_type', 'is_featured', 'is_limited_edition')
+    list_display = ('name', 'genre', 'media_type', 'price', 'stock', 'category', 'is_featured', 'is_limited_edition', 'created_at')
+    list_filter = ('category', 'genre', 'media_type', 'is_featured', 'is_limited_edition')
     search_fields = ('name', 'description')
     prepopulated_fields = {'slug': ('name',)}
     inlines = [ProductImageInline]
