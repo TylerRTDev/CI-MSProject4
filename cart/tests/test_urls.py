d18e0d39efe1ab5d930d1669d060660f17a4cfc0
TylerRTDev <TylerRTDev@outlook.com>
2025-08-23 03:46:40 +0100

    Test: Add remove_from_cart integration test to confirm item removal

--- cart/tests/test_urls.py
+++ cart/tests/test_urls.py
@@ -7,3 +7,7 @@ class CartUrlsTest(SimpleTestCase):
     def test_view_cart_url_resolves(self):
         url = reverse('cart:view_cart')
         self.assertEqual(resolve(url).func, views.view_cart)
+    
+    def test_remove_from_cart_url_resolves(self):
+        url = reverse('cart:remove_from_cart', args=['1'])
+        self.assertEqual(resolve(url).func, views.remove_from_cart)
